dad320b8411154b08ff821b7845e83ee9a8c865f
Nick Freemantle <ng.freemantle@gmail.com>
2023-10-14 21:41:31 +1100

    vim packer powermode thanks to primeagen

---
+++ .config/nvim/after/plugin/colors.lua
@@ -0,0 +1,12 @@
+
+function ColorMyPencils(color)
+	color = color or "tokyonight-moon"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" } ) 
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" } ) 
+	
+
+end
+
+ColorMyPencils()
---
+++ .config/nvim/lua/social/init.lua
@@ -0,0 +1,3 @@
+require("social.packer")
+require("social.remap")
+require("social.set")
