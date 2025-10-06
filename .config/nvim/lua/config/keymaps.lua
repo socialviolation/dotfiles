41176b58d7b7a6164420c5c60df78304c1c08983
Nick Freemantle <nick@fmtl.au>
2025-10-06 11:52:03 +1100

    Minor adjustments

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,8 +7,12 @@ vim.keymap.set("n", "W", function()
   require("bufferline.commands").cycle(-1)
   vim.cmd("bd #")
 end, { desc = "Delete buffer and go to previous" })
--- Unbind arrow keys in all modes
-vim.keymap.set({ "n", "i", "v" }, "<Up>", "<Nop>", { desc = "Disable up arrow" })
-vim.keymap.set({ "n", "i", "v" }, "<Down>", "<Nop>", { desc = "Disable down arrow" })
-vim.keymap.set({ "n", "i", "v" }, "<Left>", "<Nop>", { desc = "Disable left arrow" })
-vim.keymap.set({ "n", "i", "v" }, "<Right>", "<Nop>", { desc = "Disable right arrow" })
+-- Unbind arrow keys in normal and visual modes (but allow in insert mode)
+vim.keymap.set({ "n", "v" }, "<Up>", "<Nop>", { desc = "Disable up arrow" })
+vim.keymap.set({ "n", "v" }, "<Down>", "<Nop>", { desc = "Disable down arrow" })
+vim.keymap.set({ "n", "v" }, "<Left>", "<Nop>", { desc = "Disable left arrow" })
+vim.keymap.set({ "n", "v" }, "<Right>", "<Nop>", { desc = "Disable right arrow" })
+
+-- Move visual selection up/down with Shift+J/K
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
